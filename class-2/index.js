35971eb6f9f12cb37b4ce59258556b75f7b0f923
NehaIrfan786 <139549807+NehaIrfan786@users.noreply.github.com>
2023-07-19 03:27:10 +0500

    Add files via upload

---
+++ class-2/index.js
@@ -0,0 +1,25 @@
+//name
+var myname = "neha";
+console.log(myname);
+//age
+var age = 18;
+console.log(age);
+//address 
+var address = "ISL";
+console.log(address);
+//operators
+//+,-,*,/,%
+//addition
+var num1 = 1;
+var num2 = 9;
+console.log(num1 + num2);
+//multiplication
+console.log(num1 * num2);
+//remainder
+console.log(num1 % num2);
+//logical operators
+//&&,||,!
+//and operator
+console.log(num1 < 3 && num2 > 70);
+//or operator
+console.log(num1 > 9 || num2 < 10);
---
+++ class-2/index.ts
@@ -0,0 +1,40 @@
+//name
+let myname = "neha";
+console.log(myname);
+
+//age
+const age = 18;
+console.log(age);
+
+//address 
+var address = "ISL"
+console.log(address);
+
+//operators
+//+,-,*,/,%
+
+//addition
+
+let num1 = 1;
+let num2 = 9;
+
+console.log(num1 + num2);
+
+//multiplication
+
+console.log(num1 * num2);
+
+//remainder
+
+console.log(num1 % num2);
+
+//logical operators
+//&&,||,!
+
+//and operator
+
+console.log(num1 <3 && num2 >70);
+
+//or operator
+
+console.log(num1 > 9 || num2 < 10);
